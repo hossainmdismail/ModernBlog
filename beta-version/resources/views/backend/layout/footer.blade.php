4843c53a7a2e7d32387b79f6292b306c7736a5c6
devhamim <96724706+devhamim@users.noreply.github.com>
2023-06-10 21:50:09 +0600

    migrate

---
+++ beta-version/resources/views/backend/layout/footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="iq-footer">
+    <div class="container-fluid">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-6">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 text-right">
+                    <span class="mr-1"><script>document.write(new Date().getFullYear())</script>©</span> <a href="#" class="">POS Dash</a>.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
+<!-- Backend Bundle JavaScript -->
+<script src="../assets/js/backend-bundle.min.js"></script>
+
+<!-- Table Treeview JavaScript -->
+<script src="../assets/js/table-treeview.js"></script>
+
+<!-- Chart Custom JavaScript -->
+<script src="../assets/js/customizer.js"></script>
+
+<!-- Chart Custom JavaScript -->
+<script async src="../assets/js/chart-custom.js"></script>
+
+<!-- app JavaScript -->
+<script src="../assets/js/app.js"></script>
+</body>
+
+<!-- Mirrored from templates.iqonic.design/posdash/html/backend/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 May 2023 11:09:45 GMT -->
+</html>
