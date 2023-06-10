44e6ef9edb4ffd2a950c21a0cafbb14d2d3f1cec
Ismail <hossainmdismail9010@gmail.com>
2023-06-10 21:50:38 +0600

    update user table

--- beta-version/database/migrations/2014_10_12_000000_create_users_table.php
+++ beta-version/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('post_type',['free','premium'])->default('free');
+            $table->integer('role')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
