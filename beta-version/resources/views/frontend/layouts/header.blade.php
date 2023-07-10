cafec0a234f39602d07792cad5239cd20ec146f4
devhamim <96724706+devhamim@users.noreply.github.com>
2023-07-11 02:18:10 +0600

    blogs blade

--- beta-version/resources/views/frontend/layouts/header.blade.php
+++ beta-version/resources/views/frontend/layouts/header.blade.php
@@ -41,6 +41,9 @@
                         <a class="nav-link" href="{{ route('about') }}">About</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" href="{{ route('blogs') }}">Blogs</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="">Contact</a>
                     </li>
                     <li class="nav-item dropdown">
